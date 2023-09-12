48ba394e861bd891f5ce3c384b929013383b8690
sol2588 <dsk2588@gmail.com>
2023-09-12 21:13:16 +0900

    clinet x db connect

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import "./App.css";
+import React, { useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  useEffect(() => {
+    axios.get("http://localhost:4000/").then((res) => console.log(res));
+  }, []);
   return (
-    <div className="App">
+    <div>
       <p>This is Home</p>
     </div>
   );
